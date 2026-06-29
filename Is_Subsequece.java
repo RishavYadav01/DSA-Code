ee98c04cd39e971fdd98d2a3d53f8a86ebb526c8
Rishav Yadav <rishavyadav87421@gmail.com>
2026-06-29 23:29:45 +0530

    Implement isSubsequence method in Is_Subsequece class

---
+++ Is_Subsequece.java
@@ -0,0 +1,17 @@
+class Is_Subsequece {
+    public boolean isSubsequence(String s, String t) {
+        int i=0;
+        int j=0;
+        while(i<s.length() && j<t.length()){
+            if(s.charAt(i)==t.charAt(j)){
+                i++;
+                j++;
+            }
+            else{
+                j++;
+            }
+        }
+        return i==s.length();
+        
+    }
+}
